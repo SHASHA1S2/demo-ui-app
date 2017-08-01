b04a948bc35927a35adf155ce7b246ce17ef61c7
shashank <shashank@192.168.1.70>
2017-08-01 19:14:44 +0100

    model changes

--- src/app/article.ts
+++ src/app/article.ts
@@ -1,17 +1,5 @@
-import { Affiliation } from './affiliation';
-import { Author } from './author';
+import {ArticleMeta} from './article-meta';
 export class Article {
 
-  public journal: string;
-  public title: string;
-  public volume: number;
-  public issue: number;
-  public id: string;
-  public month: string;
-  public year: number;
-  public keywords: string[];
-  public doi: string;
-  public authors: Author[];
-  public affiliations: Affiliation[];
-  public description: string[];
+  public articleMetadata: ArticleMeta;
 }
